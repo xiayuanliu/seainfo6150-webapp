b4af4c5d6a4000d66ef875568ba0f9f0b276f382
xiayuanliu <liu.xiay@husky.neu.edu>
2020-02-12 01:02:25 -0800

    homework 5

---
+++ src/ArticleList/SlugButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SlugButton.module.css";
+
+
+const SlugButton = ({buttonText, slug}) => {
+    return(
+        <button className={styles.button} onClick={() => alert(`${slug}`)}>
+        {buttonText}
+        </button>
+    );
+}
+export default SlugButton;
